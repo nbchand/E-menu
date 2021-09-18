4554b15a5f34f7edd73730fa0a8fb700b0844b24
Narendra Chand <narenchand02@gmail.com>
2021-09-18 20:20:33 +0545

    initial commit

---
+++ src/main/java/com/ncit/emenu/EmenuApplication.java
@@ -0,0 +1,13 @@
+package com.ncit.emenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ncit/emenu/EmenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncit.emenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
